f182b521b76b1862e32b62ca53acb5fd21d53d8b
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 12:48:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
-function apiRequest(url){
-  return fetch(url)
-    .then(resp => resp.json())
-    .then(json => addImg(json))
-}
 
-
-
-function addImg(obj){
-  obj.forEach(obj => {
-    
-  })
-}
 
 document.addEventListener('DOMContentLoaded', function(){  //Listening for on page load
   apiRequest(imgUrl)

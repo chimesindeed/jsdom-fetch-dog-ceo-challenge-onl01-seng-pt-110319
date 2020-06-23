121e1c63fbad9be41e1ba1598976f0246adad017
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 12:50:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 
-
+function loadImages() {
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
 
 document.addEventListener('DOMContentLoaded', function(){  //Listening for on page load
   fetchImages();
